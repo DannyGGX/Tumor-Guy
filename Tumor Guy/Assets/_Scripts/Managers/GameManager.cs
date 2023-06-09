38b090bfc5595ca5904c706507fb75cfef48fb72
DannyGGX <danielglanz111@gmail.com>
2023-06-09 18:35:23 +0200

    Player movement and rotation

--- Tumor Guy/Assets/_Scripts/Managers/GameManager.cs
+++ Tumor Guy/Assets/_Scripts/Managers/GameManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; }
+    public bool IsPaused { get; private set; } = false;
+    public bool ControlsEnabled { get; private set; } = true;
+    public bool RobotsEnabled { get; private set; } = true;
+
+    [Header("Scene Management")]
+    [SerializeField] private int currentScene;
+    [field: Space]
+    [field: Header("Player")]
+    public List<Key> FoundKeys { get; private set; }
+    
+
+
+    void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void PauseToggle()
     {
-        
+        if (IsPaused)
+            Unpause();
+        else
+            Pause();
+    }
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        IsPaused = true;
+        ControlsEnabled = false;
+    }
+    private void Unpause()
+    {
+        Time.timeScale = 1;
+        IsPaused = false;
+        ControlsEnabled = true;
     }
 }
--- Tumor Guy/Assets/_Scripts/Player/PlayerMovement.cs
+++ Tumor Guy/Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private float movementSpeed = 5;
+    private Vector2 moveDirection;
+
+    [Space]
+    [Header("Rotation")]
+    [SerializeField] private float rotationSpeed = 15;
+    [SerializeField] private Camera camera;
+    private Quaternion rotation;
+    private const float ROTATION_OFFSET = -90;
+
+
+    private void Update()
     {
-        
+        if(GameManager.Instance.ControlsEnabled == false)
+            return;
+
+        GetInputs();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+        RotateTowardsMouse();
+    }
+
+    private void GetInputs()
+    {
+        moveDirection = GetMovementInputs();
+        rotation = GetRotationInput();
+    }
+    private Vector2 GetMovementInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        return new Vector2(moveX, moveY).normalized;
+    }
+    private Quaternion GetRotationInput()
+    {
+        Vector2 direction = camera.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + ROTATION_OFFSET;
+        return Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void Move()
+    {
+        rigidbody.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed) * Time.fixedDeltaTime;
+    }
+    private void RotateTowardsMouse()
+    {
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Dash()
+    {
+
     }
 }
